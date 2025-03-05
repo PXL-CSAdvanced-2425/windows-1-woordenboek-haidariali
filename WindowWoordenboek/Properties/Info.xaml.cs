a1c887506e5acb4366900cca5f6391d9388212e0
haidariali <ali.haidari@student.pxl.be>
2025-03-05 02:21:19 +0100

    deze Labo  werkt volledig

---
+++ WindowWoordenboek/Properties/Info.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowWoordenboek.Properties
+{
+    /// <summary>
+    /// Interaction logic for Info.xaml
+    /// </summary>
+    public partial class Info : Window
+    {
+        public Info()
+        {
+            InitializeComponent();
+            Tesktonen();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        
+        public void Tesktonen()
+        {
+            tekstTonenTextBloxk.Text = $"Productname: CS_Oefening_05\r\nVersion: 1.0.0.0\r\n\r\nCopyright: Copyright Koen Bloemen 2024\r\nCompanyname: PXL Digital\r\nDescription: In de toepassing worden specifieke\r\nEngelstalige ICT termen - Nederlandstalige termen\r\ntoegevoegd en verwijderd.";
+        }
+    }
+}
---
+++ WindowWoordenboek/Properties/ZoekenWoorden.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowWoordenboek.Properties
+{
+    /// <summary>
+    /// Interaction logic for ZoekenWoorden.xaml
+    /// </summary>
+    public partial class ZoekenWoorden : Window
+    {
+        Random random = new Random();
+        int woordNumr;
+        public ZoekenWoorden()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void ZoekenButton_Click(object sender, RoutedEventArgs e)
+        {
+            woordNumr = random.Next(0, Wachtwoorden.Engels.Count);
+
+            for (int i = 0; i < Wachtwoorden.Engels.Count; i++)
+            {
+                RamdomEngelsWoordTextBox.Text = Wachtwoorden.Engels[woordNumr];  
+            }
+
+        }
+
+        private void cotroleButton_Click(object sender, RoutedEventArgs e)
+        {
+            string ingevoerdeNederlansTerm = NederlansWoordTexbox.Text;
+            string correctNederlansWoord = Wachtwoorden.Nederlands[woordNumr];
+
+            if (ingevoerdeNederlansTerm.Equals(correctNederlansWoord, StringComparison.OrdinalIgnoreCase))
+            {
+                MessageBox.Show("U kent de vertaling correct!", "Correct", MessageBoxButton.OK, MessageBoxImage.Information);
+                NederlansWoordTexbox.Clear();
+                RamdomEngelsWoordTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Vertaling niet correct!", "Fout", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+           
+
+        }
+    }
+}
